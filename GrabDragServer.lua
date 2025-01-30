5976634da1cd345bc253193c7617a9749bc57ba3
feuzll <139976001+feuzll@users.noreply.github.com>
2025-01-30 15:46:17 +0700

    Update GrabDragServer.lua
    separate drag process for each player

--- GrabDragServer.lua
+++ GrabDragServer.lua
@@ -1,17 +1,18 @@
 print("anydrag")
-local player : Player = script.Parent
+local character : Model = script.Parent
+local player : Player = game.Players:GetPlayerFromCharacter(character)
 
 local red = workspace.Debug.red:Clone()
 local yellow = workspace.Debug.yellow:Clone()
 local green = workspace.Debug.green:Clone()
 
-local root : BasePart = player:WaitForChild("HumanoidRootPart")
+local root : BasePart = character:WaitForChild("HumanoidRootPart")
 
 red.CFrame = root.CFrame
 red.Parent = root
 
 local pack = Instance.new("Folder")
-pack.Name = player.Name .. " debug"
+pack.Name = character.Name .. " debug"
 pack.Parent = workspace
 
 yellow.Parent = pack
@@ -26,15 +27,16 @@ local grabattach : Attachment = nil
 
 
 local function YellowToCamera(owner : Player, cameraframe : CFrame)
+	if owner ~= player then return end
 	local offset = CFrame.new(0,0,-grabDistance)
 	yellow.CFrame = cameraframe:ToWorldSpace(offset)
 end
 
-local function StickGreen(owner : Player, cameraframe : CFrame, cast : Ray)
+local function TryGrab(owner : Player, cameraframe : CFrame, cast : Ray)
 	local Parameters = RaycastParams.new()
 	Parameters.FilterType = Enum.RaycastFilterType.Exclude
 	Parameters.FilterDescendantsInstances = {owner.Character}
-	
+
 	local Distance = 32
 
 	local Raycast = game.Workspace:Raycast(cast.Origin, cast.Direction*Distance, Parameters)
@@ -47,21 +49,24 @@ local function StickGreen(owner : Player, cameraframe : CFrame, cast : Ray)
 				grabpart = Raycast.Instance
 				local desiredPivotCFrameInWorldSpace = green.CFrame
 				grabpart.PivotOffset = grabpart.CFrame:ToObjectSpace(desiredPivotCFrameInWorldSpace)
-				
+
 				grabattach = Instance.new("Attachment")
 				grabattach.Name = "grabattach"
 				grabattach.Parent = grabpart
 				grabattach.WorldCFrame = grabpart:GetPivot()
-				
+
+
 				alignerp = Instance.new("AlignPosition")
+				alignerp.Name = "alignerposit"
 				alignerp.Parent = pack
 				alignerp.MaxForce = 10000000
 				alignerp.MaxAxesForce = Vector3.new(10000000,10000000,10000000) 
 				alignerp.Responsiveness = 50
 				alignerp.Attachment0 = grabattach
 				alignerp.Attachment1 = draghere
-				
+
 				alignero = Instance.new("AlignOrientation")
+				alignerp.Name = "alignerorient"
 				alignero.Parent = pack
 				alignero.Attachment0 = grabattach
 				alignero.Attachment1 = draghere
@@ -70,20 +75,41 @@ local function StickGreen(owner : Player, cameraframe : CFrame, cast : Ray)
 	end
 end
 
+local function ungrab(owner : Player)
+	if owner ~= player then return end
+	grabpart = nil
+	alignerp:Destroy()
+	alignero:Destroy()
+	alignerp = nil
+	alignero = nil
+end
+
+local function LeftClick(owner : Player, cameraframe : CFrame, cast : Ray)
+	if owner ~= player then return end
+	
+	print("left click from " .. character.Name)
+	if not alignerp then
+		TryGrab(owner,cameraframe,cast)
+	else
+		ungrab(owner)
+	end
+	
+	
+end
+
+
+
 
 local function MoveGrabbed()
 	if grabpart ~= nil then
 		--grabpart:PivotTo(yellow.CFrame)
+		green.CFrame = grabpart:GetPivot()
 	end
 end
 
-local function ungrab()
-	grabpart = nil
-	alignerp:Destroy()
-	alignero:Destroy()
-end
+
 
 game.ReplicatedStorage.CameraUpdate.OnServerEvent:Connect(YellowToCamera)
-game.ReplicatedStorage.HitGrab.OnServerEvent:Connect(StickGreen)
+game.ReplicatedStorage.HitGrab.OnServerEvent:Connect(LeftClick)
 game.ReplicatedStorage.UnGrab.OnServerEvent:Connect(ungrab)
 game["Run Service"].Heartbeat:Connect(MoveGrabbed)
