aac78f99a0df1ab13216a8b24555cadfa290aa22
feuzll <139976001+feuzll@users.noreply.github.com>
2025-02-22 11:11:59 +0700

    add beam visual at server side

--- GrabDragServer.lua
+++ GrabDragServer.lua
@@ -30,6 +30,7 @@ local function YellowToCamera(owner : Player, cameraframe : CFrame)
 	if owner ~= player then return end
 	local offset = CFrame.new(0,0,-grabDistance)
 	yellow.CFrame = cameraframe:ToWorldSpace(offset)
+	yellow.CFrame = CFrame.fromMatrix(yellow.Position,yellow.CFrame.RightVector, workspace.WorldPivot.UpVector)
 end
 
 local function TryGrab(owner : Player, cameraframe : CFrame, cast : Ray)
@@ -45,6 +46,7 @@ local function TryGrab(owner : Player, cameraframe : CFrame, cast : Ray)
 		if Raycast.Instance then 
 			grabDistance = Raycast.Distance
 			green.CFrame = CFrame.lookAlong(Raycast.Position, cameraframe.LookVector)
+			green.CFrame = CFrame.fromMatrix(green.Position,green.CFrame.RightVector, workspace.WorldPivot.UpVector)
 			if Raycast.Instance:IsA("BasePart") then 
 				grabpart = Raycast.Instance
 				local desiredPivotCFrameInWorldSpace = green.CFrame
@@ -68,6 +70,7 @@ local function TryGrab(owner : Player, cameraframe : CFrame, cast : Ray)
 				alignero = Instance.new("AlignOrientation")
 				alignerp.Name = "alignerorient"
 				alignero.Parent = pack
+				alignero.MaxTorque = 10000000
 				alignero.Attachment0 = grabattach
 				alignero.Attachment1 = draghere
 			end
@@ -107,9 +110,63 @@ local function MoveGrabbed()
 	end
 end
 
+local BezierPath =require(game.ReplicatedStorage["BezierPath2.0"])
+local Positions = {
+	red.Position,
+	yellow.Position,
+	green.Position
+}
+local visParts = {}
+local NewPath = BezierPath.new(Positions,3)
+local visFolder = Instance.new("Folder")
+visFolder.Name = player.Name .. " arrows"
+visFolder.Parent = workspace
+for T = 0.0,1.0,1/30.0 do
+	local arrowCopy = workspace.Debug.arrow:Clone()
+	arrowCopy.Parent = visFolder
+	arrowCopy.CFrame = NewPath:CalculateUniformCFrame(T)
+	table.insert(visParts,arrowCopy)
+end
 
+local beamRY = workspace.Debug.BeamRY
+local beamYG = workspace.Debug.BeamYG
+beamRY.Attachment0 = root.RootAttachment
+
+
+local function DrawGrabHint()
+	--Positions = {
+	--	red.Position,
+	--	yellow.Position,
+	--	green.Position
+	--}
+	--NewPath = BezierPath.new(Positions,3)
+
+	--for index, value in visParts do
+	--	value.CFrame = NewPath:CalculateUniformCFrame(index/30.0)
+	--end
+	beamRY.Attachment1 = draghere
+	beamYG.Attachment0 = draghere
+	beamYG.Attachment1 = grabattach
+end
+
+local function UpdateCurve(player : Player, delta : Vector2)
+	if math.sign(delta.X) == math.sign(beamRY.CurveSize1) then 
+		beamRY.CurveSize1 = 0
+	end
+	beamRY.CurveSize1 -= delta.X/100.0
+	beamRY.CurveSize1 = math.clamp(beamRY.CurveSize1,-10,10)
+end
+
+local function ReachCurveZero()
+	--beamRY.CurveSize1 = (math.abs(beamRY.CurveSize1) - 0.01) * math.sign(beamRY.CurveSize1)
+	--local result = math.lerp(beamRY.CurveSize1, 0, 0.05)
+	--beamRY.CurveSize1 = result
+end
 
 game.ReplicatedStorage.CameraUpdate.OnServerEvent:Connect(YellowToCamera)
 game.ReplicatedStorage.HitGrab.OnServerEvent:Connect(LeftClick)
 game.ReplicatedStorage.UnGrab.OnServerEvent:Connect(ungrab)
+game.ReplicatedStorage.MouseUpdate.OnServerEvent:Connect(UpdateCurve)
 game["Run Service"].Heartbeat:Connect(MoveGrabbed)
+game["Run Service"].Heartbeat:Connect(DrawGrabHint)
+game["Run Service"].Heartbeat:Connect(ReachCurveZero)
