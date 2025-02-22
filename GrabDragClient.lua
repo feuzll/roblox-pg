b6c715b1b90d3894e1f5755e0ea1314a4b8cb69d
feuzll <139976001+feuzll@users.noreply.github.com>
2025-02-22 11:12:39 +0700

    add drag-beam visual at client side

--- GrabDragClient.lua
+++ GrabDragClient.lua
@@ -21,6 +21,12 @@ local function Ungrab()
 	game.ReplicatedStorage.UnGrab:FireServer()
 end
 
+local function OnMouseUpdate()
+	local delta :Vector2 = game.UserInputService:GetMouseDelta() 
+	game.ReplicatedStorage.MouseUpdate:FireServer(delta)
+end
+
 mouse.Button2Down:Connect(Ungrab)
 mouse.Button1Down:Connect(PassClickInfo)
 game["Run Service"].Heartbeat:Connect(PassCameraFrame)
+game["Run Service"]:BindToRenderStep("whatever", Enum.RenderPriority.Input.Value, OnMouseUpdate)
